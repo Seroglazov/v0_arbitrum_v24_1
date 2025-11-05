aadf7393a465a7dd1dced8ec61f4c9063bfef937
Seroglazov <ivanser2000@gmail.com>
2025-11-05 13:37:11 +0300

    Updated trace

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/88134d00f828eb57-s.p.woff2\",\"static/media/aa924b7c62245c84-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
